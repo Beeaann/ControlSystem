726f918664074c8282dbef5d082ea0c629e31be0
Luke Slautterback <lukeslautterback@gmail.com>
2026-04-21 03:06:29 -0400

    Enabled native Raspberry Pi 5 camera logging

--- src/camera/camera_logger.cpp
+++ src/camera/camera_logger.cpp
@@ -1,6 +1,7 @@
 #include "camera/camera_logger.hpp"
 #include <iostream>
 #include <cstdlib>
+#include <string>
 
 namespace camera {
 
@@ -8,8 +9,10 @@ CameraLogger::CameraLogger() : is_initialized_(true) {}
 
 void CameraLogger::captureEvent(const std::string& eventReason) {
     std::cout << "[CAMERA] Capturing triggered image log: " << eventReason << "\n";
-    // Command line utility triggers image natively
-    // system(("libcamera-still -o event_log_" + eventReason + ".jpg --immediate &").c_str());
+    
+    // Fire the native Pi 5 camera command to save a JPEG instantly upon fault
+    std::string command = "libcamera-still -o fault_log_" + eventReason + ".jpg --immediate &";
+    system(command.c_str());
 }
 
 } // namespace camera
